3614b6c071ffa4c453146860388a9df412a4e227
emidio@morgia.info <emidio@morgia.info>
2023-04-15 15:56:00 +0200

    Values controller

---
+++ src/ApiGateways/Web/Web.BFF/Web.BFF/ValueItem.cs
@@ -0,0 +1,14 @@
+﻿namespace Web.BFF
+{
+    public class ValueItem
+    {
+        public int Id { get; set; }
+        public string Value { get; set; }
+
+        public ValueItem(int id, string value)
+        {
+            Id = id;
+            Value = value;
+        }
+    }
+}
